8635142faed9eb9f8783a8f73b928068a56e1c9e
Evan Oman <evan058@gmail.com>
2015-10-23 18:33:23 -0500

    Completed analysis, working on writeup

--- analysis.R
+++ analysis.R
@@ -1,25 +1,124 @@
-setwd("C:/cygwin/home/Oman/PML")
+# setwd("C:/cygwin/home/Oman/PML")
+# library(caret)
+# allData <- read.csv("./pml-training.csv", na.strings=c("","NA"))
+# 
+# # Username, row nand timestamps are probabaly not good indicators
+# allData <- subset(allData, select = -c(user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window,new_window,X))
+# 
+# for (i in names(allData))
+# {
+#   if (i != "classe")
+#   {
+#     allData[[i]] <- as.numeric(allData[[i]])  
+#   }
+# 
+# }
+# # Gets the number of rows
+# nObs <- nrow(allData)
+# 
+# # We will now remove all predictors for which there are more than 25% NA's
+# allData <- allData[,colSums(is.na(allData))/nObs < 0.25]
+# 
+# # Exploratory analysis:
+# library(reshape2)
+# library(ggplot2)
+# d <- melt(data.frame(scale(allData[,1:(ncol(allData)-1)])))
+# pdf("hist.pdf", height=20,width=20)
+# ggplot(d,aes(x = value)) + 
+#   facet_wrap(~variable,scales = "free_x") + 
+#   geom_histogram() +
+#   ggtitle("Scaled Histograms of All Predictors")
+# dev.off()
+# 
+# inTrain <- createDataPartition(y=allData$classe,p=.7,list=FALSE)
+# training <- allData[inTrain,]
+# testing <- allData[-inTrain,]
+# 
+# #Fitting the model
+# myControl <- trainControl(method="cv", 5)
+# #modFit <- train(classe ~ ., method="rf", data=training, verbose=FALSE, trControl = myControl, ntree=250)
+# #print(modFit)
+# 
+# # Make predictions
+# preds <- predict(modFit, testing)
+# 
+# # Print accuracy
+# confusionMatrix(preds,testing$classe)
+# 
+# # Save off our R model
+# saveRDS(modFit, "mySavedModel001.rds")
+# # modFit <- readRDS("mySavedModel001.rds")
+# 
+# # Plot the results
+# # pdf("boostingPlot.pdf")
+# # qplot(predict(modFit,testing),wage,data=testing)
+# # dev.off()
+
+
+
+
+
+
+## FROME OTHER FILE
 library(caret)
-allData <- read.csv("./pml-training.csv")
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(reshape2)
+library(ggplot2)
 
-# Username and timestamps are probabaly not good indicators
-allData <- subset(allData, select = -c(user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp))
+allData <- read.csv("./pml-training.csv", na.strings=c("","NA"))
+subTest <- read.csv("./pml-testing.csv")
+
+for (i in names(allData[,1:(ncol(allData)-1)]))
+{
+  allData.cleaned[[i]] <- as.numeric(allData[[i]])
+}
+
+# Gets the number of rows
+nObs <- nrow(allData.cleaned)
+
+# We will now remove all predictors for which there are more than 25% NA's
+allData.cleaned <- allData.cleaned[,colSums(is.na(allData))/nObs < 0.25]
 
 
-inTrain <- createDataPartition(y=allData$classe,p=.7,list=FALSE)
-training <- allData[inTrain,]
-testing <- allData[-inTrain,]
+inTrain <- createDataPartition(y=allData.cleaned$classe,p=.7,list=FALSE)
+training <- allData.cleaned[inTrain,]
+testing <- allData.cleaned[-inTrain,]
 
-# Fitting the model
-modFit <- train(classe ~ ., method="gbm", data=training, verbose=FALSE)
-print(modFit)
+
+
+myControl <- trainControl(method="cv", 5)
+modFit <- train(classe ~ ., method="rf", data=training, verbose=FALSE, trControl = myControl, ntree=250)
+
 
 # Make predictions
 preds <- predict(modFit, testing)
 
-# Print accuracy
 
-# Plot the results
-# pdf("boostingPlot.pdf")
-# qplot(predict(modFit,testing),wage,data=testing)
-# dev.off()
+
+
+
+# Print accuracy
+confusionMatrix(preds,testing$classe)
+
+
+
+# First we clean the submission test set
+myPredictors <- names(allData.cleaned)
+myPredictors <- myPredictors[myPredictors!= "classe"]
+subTest <- subTest[,myPredictors]
+# Make predictions on submission test set
+predsSub <- predict(modFit, subTest)
+
+
+# Make predictions on 
+confusionMatrix(predsSub,testing$classe)
+
+
+
+d <- melt(data.frame(scale(allData[,1:(ncol(allData)-1)])))
+ggplot(d,aes(x = value)) + 
+  facet_wrap(~variable,scales = "free_x", ncol = 4) + 
+  geom_histogram() +
+  ggtitle("Scaled Histograms of All Predictors")
